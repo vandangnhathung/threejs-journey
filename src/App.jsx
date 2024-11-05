210a632cb582600f543dc47b69881e98b3fe96b7
Pau15122 <77733880+vandangnhathung@users.noreply.github.com>
2024-11-05 09:35:19 +0700

    feat(lesson2): learn about Vector3, Euler

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import Lesson2 from "./lessons/02/Lesson2.jsx";
 import Lesson1 from "./lessons/01/Lesson1.jsx";
 
 function App(){
 
     return (
         <>
-            <Lesson1/>
+            {/*<Lesson1/>*/}
+            <Lesson2/>
         </>
     )
 }
