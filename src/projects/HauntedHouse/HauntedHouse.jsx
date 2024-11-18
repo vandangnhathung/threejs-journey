3e16e9c1fa63b409a5eac20699bbbfba8646c936
Pau15122 <77733880+vandangnhathung@users.noreply.github.com>
2024-11-19 00:46:43 +0700

    feat(haunted-house): Set up geometries

--- src/projects/HauntedHouse/HauntedHouse.jsx
+++ src/projects/HauntedHouse/HauntedHouse.jsx
@@ -4,7 +4,7 @@ import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
 import GUI from 'lil-gui'
 import {Timer} from "three/addons";
 
-const Lesson15 = () => {
+const HauntedHouse = () => {
 
     // Get canvas element
     const canvasRef = useRef(null);
@@ -27,12 +27,87 @@ const Lesson15 = () => {
         /**
          * House
          */
-            // Temporary sphere
-        const sphere = new THREE.Mesh(
-                new THREE.SphereGeometry(1, 32, 32),
+
+            // Floor
+        const floor = new THREE.Mesh(
+                new THREE.PlaneGeometry(20, 20),
                 new THREE.MeshStandardMaterial({roughness: 0.7})
-            )
-        scene.add(sphere)
+            );
+        floor.rotation.x = -Math.PI * 0.5
+        scene.add(floor);
+
+        const house = new THREE.Group();
+        scene.add(house);
+
+        // Walls
+        const walls = new THREE.Mesh(
+            new THREE.BoxGeometry(4, 2.5, 4),
+            new THREE.MeshStandardMaterial()
+        );
+        walls.position.y = 1.25;
+        house.add(walls);
+
+        // Roof
+        const roof = new THREE.Mesh(
+            new THREE.ConeGeometry(3.5, 1, 4),
+            new THREE.MeshStandardMaterial()
+        );
+        roof.position.y = 2.5 + 0.5;
+        roof.rotation.y = Math.PI * 0.25;
+        house.add(roof);
+
+        // Door
+        const door = new THREE.Mesh(
+            new THREE.BoxGeometry(1, 2, 0.1),
+            new THREE.MeshStandardMaterial()
+        );
+        door.position.z = 2;
+        door.position.y = 1;
+        house.add(door);
+
+        // Bushes
+        const bushGeometry = new THREE.SphereGeometry(1, 16, 16);
+        const bushMaterial = new THREE.MeshStandardMaterial({color: '#89c854'});
+        const bush1 = new THREE.Mesh(bushGeometry, bushMaterial);
+        bush1.scale.set(0.5, 0.5, 0.5);
+        bush1.position.set(0.8, 0.2, 2.2);
+        house.add(bush1);
+
+        const bush2 = new THREE.Mesh(bushGeometry, bushMaterial);
+        bush2.scale.set(0.25, 0.25, 0.25);
+        bush2.position.set(1.4, 0.1, 2.1);
+        house.add(bush2);
+
+        const bush3 = new THREE.Mesh(bushGeometry, bushMaterial);
+        bush3.scale.set(0.4, 0.4, 0.4);
+        bush3.position.set(-1, 0.1, 2.2);
+        house.add(bush3);
+
+        const bush4 = new THREE.Mesh(bushGeometry, bushMaterial);
+        bush4.scale.set(0.15, 0.15, 0.15);
+        bush4.position.set(-1.5, 0.05, 2.6);
+        house.add(bush4);
+
+        // Graves
+        const graves = new THREE.Group();
+        scene.add(graves);
+
+        const graveGeometry = new THREE.BoxGeometry(0.6, 0.8, 0.2);
+        const graveMaterial = new THREE.MeshStandardMaterial({color: '#b2b6b1'});
+        for(let i = 0; i < 50; i++){
+            const angle = Math.random() * Math.PI * 2;
+            const radius = 3 + Math.random() * 6;
+            const x = Math.cos(angle) * radius;
+            const z = Math.sin(angle) * radius;
+
+            const grave = new THREE.Mesh(graveGeometry, graveMaterial);
+            grave.position.set(x, 0.4, z);
+            grave.rotation.y = (Math.random() - 0.5) * 0.4;
+            grave.rotation.z = (Math.random() - 0.5) * 0.4;
+            grave.castShadow = true;
+            graves.add(grave);
+        }
+
 
         /**
          * Lights
@@ -73,9 +148,9 @@ const Lesson15 = () => {
          */
             // Base camera
         const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-        camera.position.x = 4
-        camera.position.y = 2
-        camera.position.z = 5
+        camera.position.x = 10
+        camera.position.y = 4
+        camera.position.z = 12
         scene.add(camera)
 
         // Controls
@@ -123,4 +198,4 @@ const Lesson15 = () => {
     );
 };
 
-export default Lesson15;
+export default HauntedHouse;
